b01fe9348e3a929856308f10dc138638e55b8f9d
aniyer <aniyer@tibco.com>
2021-07-09 23:28:07 +0530

    Added the following changes: - Exposed routes - Added an Executor function - Added a struct placeholder
    Tested locally with both routes executeJob & viewJob

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	_ "expvar"
+	"log"
+	"net/http"
+	_ "net/http/pprof"
+
+	"github.com/anandiyergit/job-runner/server"
+)
 
 func main() {
-	fmt.Println("I am from go mod")
+	// handler for jobStore
+	handlers := server.JobStore{}
+
+	// Defining the routes
+
+	// For simplicity to use GET /executeJob. In ideal world this should be a post request but just for the sake of ease of test.
+	http.HandleFunc("/executeJob", handlers.ExecuteJob)
+
+	// Route for Viewing the Finished Jobs
+	http.HandleFunc("/viewJobs", handlers.ViewJobs)
+
+	log.Fatal(http.ListenAndServe("localhost:8080", nil))
 }
